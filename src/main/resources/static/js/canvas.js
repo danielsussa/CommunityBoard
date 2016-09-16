3a1f368e8b9961905ea906f5216cd84edd64d2e7
Daniel Kanczuk <danielsussa@gmail.com>
2016-09-16 10:33:38 -0300

    Separado o html do js

---
+++ src/main/resources/static/js/canvas.js
@@ -0,0 +1,46 @@
+/**
+ * Created by daniel on 16/09/16.
+ */
+
+
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+var isDrawing;
+ctx.globalAlpha = 1;
+
+
+canvas.onmousedown = function(e) {
+    isDrawing = true;
+    ctx.beginPath();
+    ctx.moveTo(e.offsetX, e.offsetY);
+
+};
+canvas.onmousemove = function(e) {
+    if (isDrawing) {
+        ctx.lineTo(e.offsetX, e.offsetY);
+        ctx.stroke();
+    }
+};
+canvas.onmouseup = function() {
+    isDrawing = false;
+    ctx.closePath();
+    var data = canvas.toDataURL();
+    $.post("/save",{ canvas: data} );
+};
+
+
+
+function getContent(){
+    $.get("/getCanvas",function (canvasData) {
+        if(canvasData){
+            var myCanvas = document.getElementById('canvas');
+            var ctx = myCanvas.getContext('2d');
+            var img = new Image;
+            img.onload = function(){
+                ctx.drawImage(img,0,0); // Or at whatever offset you like
+            };
+            img.src = canvasData;
+        }
+    });
+}
+setInterval(getContent, 2000);
